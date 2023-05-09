9ed590221b0156d2ac7ca61405f898f4749156b7
arseniov <83009979+arseniov@users.noreply.github.com>
2023-05-09 18:47:19 +0200

    commit

--- 01_Day_Introduction/01.py
+++ 01_Day_Introduction/01.py
@@ -1,1 +1,23 @@
-# test
+# Python course
+# Day 1
+
+print("Giacomo Viganò")
+
+print(type(5))
+
+print(type("Ciao!"))
+
+print(type(["One","Two","Three!"]))
+
+print(5 == 5)
+print(5 == 6)
+
+print({
+    "first":"Louis",
+    "family":"Armstrong",
+    "married":False,
+    "skills":["React","JS","Python"]
+})
+
+print("{3,4,1,6} is " & type(5))
+
